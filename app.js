c2a2335e4fb23ad53071e5de62cb5e3bbc524d7a
winderaranguren-gif <winder.aranguren@gmail.com>
2026-08-06 22:43:24 -0400

    refactor: run legacy data migration before restored suite

--- app.js
+++ app.js
@@ -3,8 +3,18 @@
   css.rel='stylesheet';
   css.href='atlas-suite.css?v=4';
   document.head.append(css);
-  const script=document.createElement('script');
-  script.src='atlas-suite.js?v=4';
-  script.async=false;
-  document.body.append(script);
+
+  const loadSuite=()=>{
+    const script=document.createElement('script');
+    script.src='atlas-suite.js?v=4';
+    script.async=false;
+    document.body.append(script);
+  };
+
+  const migration=document.createElement('script');
+  migration.src='atlas-legacy-migrate.js?v=4';
+  migration.async=false;
+  migration.onload=loadSuite;
+  migration.onerror=loadSuite;
+  document.body.append(migration);
 })();
