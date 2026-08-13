9cdb1b96c7b3d366aeeec6e8f60b90752a539013
winderaranguren-gif <winder.aranguren@gmail.com>
2026-08-12 23:13:55 -0400

    Add ATLAS HR Spatial service worker

---
+++ modules/hr-spatial-app/sw.js
@@ -0,0 +1,1 @@
+const C='atlas-hr-spatial-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
